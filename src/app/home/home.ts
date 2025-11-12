05879787b1173bbe7f1487c124b87804638a3003
Dani S. <daniela.schomaker@posteo.de>
2025-11-12 13:33:26 +0100

    feat: add basic home structure

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero/hero';
+import { AboutMe } from './about-me/about-me';
+import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
+import { Testimonials } from './testimonials/testimonials';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Hero, AboutMe, Skills, Projects, Testimonials, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
