45350c1f50a8b0995322688097c6082377da7563
Dani S. <daniela.schomaker@posteo.de>
2025-11-12 13:32:27 +0100

    feat: add RouterLink

---
+++ src/app/shared/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+// import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [], // Muss hier der RouterLink rein?
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+
+}
