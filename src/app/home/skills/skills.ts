9170c6506a846d55939bb61da4c3454d96c47908
Dani S. <daniela.schomaker@posteo.de>
2025-11-18 17:09:34 +0100

    feat: add skill icons

--- src/app/home/skills/skills.ts
+++ src/app/home/skills/skills.ts
@@ -7,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrl: './skills.scss',
 })
 export class Skills {
-
+  skills = [
+    {
+      name: 'HTML',
+      src: 'img/icons/skills/html.svg',
+    },
+    {
+      name: 'CSS',
+      src: 'img/icons/skills/css.svg',
+    },
+    {
+      name: 'JavaScript',
+      src: 'img/icons/skills/javaScript.svg',
+    },
+    {
+      name: 'Material Design',
+      src: 'img/icons/skills/materialDesign.svg',
+    },
+    {
+      name: 'TypeScript',
+      src: 'img/icons/skills/typeScript.svg',
+    },
+    {
+      name: 'Angular',
+      src: 'img/icons/skills/angular.svg',
+    },
+    {
+      name: 'Firebase',
+      src: 'img/icons/skills/firebase.svg',
+    },
+    {
+      name: 'Git',
+      src: 'img/icons/skills/git.svg',
+    },
+    {
+      name: 'REST-API',
+      src: 'img/icons/skills/restApi.svg',
+    },
+    {
+      name: 'Scrum',
+      src: 'img/icons/skills/scrum.svg',
+    },
+    {
+      name: 'Growth mindset',
+      src: 'img/icons/skills/growthMindset.svg',
+    },
+  ];
 }
