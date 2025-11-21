8cef0bd1edeaafc3bfdfbfe5d4a2e0cd9232c0ed
Dani S. <daniela.schomaker@posteo.de>
2025-11-21 10:16:20 +0100

    feat: add Project interface

---
+++ src/app/interfaces/project.interface.ts
@@ -0,0 +1,11 @@
+export interface Project {
+//   id: number;
+  number: number; // geht das mit der Null?
+  title: string;
+  description: string;
+  tech: string[];
+  image: string;
+  github: string;
+  live: string;
+}
+
