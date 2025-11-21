5556df43e75a11d7dac1ef53276c51ac01ab85c7
Dani S. <daniela.schomaker@posteo.de>
2025-11-21 10:16:09 +0100

    feat: add dialog component

--- src/app/home/projects/projects.ts
+++ src/app/home/projects/projects.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Dialog } from './dialog/dialog';
+import { Project } from '../../interfaces/project.interface';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [Dialog],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
