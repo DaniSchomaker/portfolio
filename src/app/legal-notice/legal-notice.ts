14e473e484f22a82e948dcdc2a11e76abe2cb7f0
Dani S. <daniela.schomaker@posteo.de>
2025-11-24 17:55:04 +0100

    feat: add legal-notice page

--- src/app/legal-notice/legal-notice.ts
+++ src/app/legal-notice/legal-notice.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-legal-notice',
   imports: [],
+  // standalone: true,
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
 export class LegalNotice {
 
+  ngOnInit() {
+    document.body.classList.add('legal-notice-bg');
+  }
+
+  ngOnDestroy() {
+    document.body.classList.remove('legal-notice-bg');
+  }
 }
