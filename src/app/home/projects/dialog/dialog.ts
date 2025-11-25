76edde6ca6752e543e45c04381fa369a22e10a54
Dani S. <daniela.schomaker@posteo.de>
2025-11-25 17:56:43 +0100

    feat: add click-events for dialog (open/close/next)

--- src/app/home/projects/dialog/dialog.ts
+++ src/app/home/projects/dialog/dialog.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { Component, EventEmitter, Input, Output } from '@angular/core';
+// import { Project } from '../../../interfaces/project.interface';
+
+// @Component({
+//   selector: 'app-dialog',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './dialog.html',
+//   styleUrl: './dialog.scss',
+// })
+// export class Dialog {
+//   @Input() project!: Project;
+//   @Output() close = new EventEmitter<void>();
+
+//   onOverlayClick() {
+//     this.close.emit();
+//   }
+
+//   onDialogClick(event: MouseEvent) {
+//     event.stopPropagation();
+//   }
+// }
+
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Project } from '../../../interfaces/project.interface';
 
 @Component({
   selector: 'app-dialog',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dialog.html',
   styleUrl: './dialog.scss',
 })
 export class Dialog {
+  @Input() project!: Project;
+  @Output() close = new EventEmitter<void>();
+  @Output() next = new EventEmitter<void>();  
 
+  onOverlayClick() {
+    this.close.emit();
+  }
+
+  onDialogClick(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
+  onNextClick(event: MouseEvent) {
+    event.stopPropagation();   // klick soll NICHT den Overlay-Click auslösen
+    this.next.emit();
+  }
 }
+
