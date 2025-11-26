938f85560035f6461f186cfcd5c0f42546dfad97
Dani S. <daniela.schomaker@posteo.de>
2025-11-26 13:17:48 +0100

    refactor: delete Testimonials

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,12 +3,11 @@ import { Hero } from './hero/hero';
 import { AboutMe } from './about-me/about-me';
 import { Skills } from './skills/skills';
 import { Projects } from './projects/projects';
-import { Testimonials } from './testimonials/testimonials';
 import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, AboutMe, Skills, Projects, Testimonials, Contact],
+  imports: [Hero, AboutMe, Skills, Projects, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
