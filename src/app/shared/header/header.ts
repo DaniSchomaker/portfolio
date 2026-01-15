30dde986f5c830c5a48eeb1e09ef158c63669a92
Dani S. <daniela.schomaker@posteo.de>
2026-01-15 13:47:21 +0100

    feat: add burger-menu

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,16 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [TranslatePipe],
+  standalone: true,
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   private translate = inject(TranslateService);
 
-  currentLanguage: 'en' | 'de' = 'en';   
+  currentLanguage: 'en' | 'de' = 'en';
+  menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
 
   useLanguage(language: 'en' | 'de'): void {
     this.currentLanguage = language;
@@ -20,4 +31,3 @@ export class Header {
     document.documentElement.classList.add('language-' + language);
   }
 }
-
