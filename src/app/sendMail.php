b57d38225fe4c5badc464999adf8ab45c99a5e2d
Dani S. <daniela.schomaker@posteo.de>
2026-01-19 12:55:41 +0100

    content: change email-address

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'daniela.schomaker@posteo.de';  
+            $recipient = 'kontakt@taxedtech.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
