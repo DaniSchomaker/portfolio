d7fdc99dc5280aa14ea76de70e16ba74d8340bf8
Dani S. <daniela.schomaker@posteo.de>
2026-01-20 13:14:50 +0100

    style(dialog): change dialog styling

--- src/app/home/projects/dialog/dialog.ts
+++ src/app/home/projects/dialog/dialog.ts
@@ -1,27 +1,3 @@
-// import { CommonModule } from '@angular/common';
-// import { Component, EventEmitter, Input, Output } from '@angular/core';
-// import { Project } from '../../../interfaces/project.interface';
-
-// @Component({
-//   selector: 'app-dialog',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './dialog.html',
-//   styleUrl: './dialog.scss',
-// })
-// export class Dialog {
-//   @Input() project!: Project;
-//   @Output() close = new EventEmitter<void>();
-
-//   onOverlayClick() {
-//     this.close.emit();
-//   }
-
-//   onDialogClick(event: MouseEvent) {
-//     event.stopPropagation();
-//   }
-// }
-
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Project } from '../../../interfaces/project.interface';
@@ -47,7 +23,7 @@ export class Dialog {
   }
 
   onNextClick(event: MouseEvent) {
-    event.stopPropagation();   // klick soll NICHT den Overlay-Click auslösen
+    event.stopPropagation();   
     this.next.emit();
   }
 }
