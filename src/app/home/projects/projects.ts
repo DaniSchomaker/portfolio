9539175071d1fc099b509cf7e93dcff05f3969c1
Dani S. <daniela.schomaker@posteo.de>
2026-01-27 12:21:18 +0100

    refactor(projects): move PROJECTS constant out of interface into projects module

--- src/app/home/projects/projects.ts
+++ src/app/home/projects/projects.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { PROJECTS, Project } from '../../interfaces/project.interface';
-import { Dialog } from './dialog/dialog';
 import { TranslatePipe } from '@ngx-translate/core';
+
+import { Dialog } from './dialog/dialog';
+import { Project } from '../../interfaces/project.interface';
 
 type ProjectTechEntry = {
   name: string;
@@ -10,12 +11,60 @@ type ProjectTechEntry = {
 };
 
 /**
+ * Static list of available projects.
+ * Used by the projects overview and dialog.
+ */
+const PROJECTS: Project[] = [
+  {
+    id: '01',
+    title: 'Pokédex',
+    tech: [
+      { icon: 'img/icons/dialog-javascript.svg', name: 'JavaScript' },
+      { icon: 'img/icons/dialog-html.svg', name: 'HTML' },
+      { icon: 'img/icons/dialog-css.svg', name: 'CSS' },
+    ],
+    shortDescriptionKey: 'projects.items.pokedex.shortDescription',
+    image: 'img/projects/pokedex.png',
+    github: 'https://github.com/DaniSchomaker/Pokedex',
+    live: 'https://pokedex.taxedtech.de',
+  },
+  {
+    id: '02',
+    title: 'El Pollo Loco',
+    tech: [
+      { icon: 'img/icons/dialog-javascript.svg', name: 'JavaScript' },
+      { icon: 'img/icons/dialog-html.svg', name: 'HTML' },
+      { icon: 'img/icons/dialog-css.svg', name: 'CSS' },
+    ],
+    shortDescriptionKey: 'projects.items.el-pollo-loco.shortDescription',
+    image: 'img/projects/el-pollo-loco.png',
+    github: 'https://github.com/DaniSchomaker/El_Pollo_Loco',
+    live: 'https://el-pollo-loco.taxedtech.de',
+  },
+  {
+    id: '03',
+    title: 'Join',
+    tech: [
+      { icon: 'img/icons/dialog-angular.svg', name: 'Angular' },
+      { icon: 'img/icons/dialog-typescript.svg', name: 'TypeScript' },
+      { icon: 'img/icons/dialog-firebase.svg', name: 'Firebase' },
+      { icon: 'img/icons/dialog-html.svg', name: 'HTML' },
+      { icon: 'img/icons/dialog-css.svg', name: 'CSS' },
+    ],
+    shortDescriptionKey: 'projects.items.join.shortDescription',
+    image: 'img/projects/join.png',
+    github: 'https://github.com/DaniSchomaker/Join',
+    live: 'https://join.taxedtech.de',
+  },
+];
+
+/**
  * Projects overview component.
  *
  * Responsibilities:
  * - Renders a list of projects
  * - Opens a dialog with details for the selected project
- * - Supports navigation to the next project within the dialog
+ * - Allows navigation to the next project
  * - Provides small view helpers for template bindings
  */
 @Component({
@@ -25,9 +74,7 @@ type ProjectTechEntry = {
   styleUrl: './projects.scss',
 })
 export class Projects {
-  /**
-   * Static list of available projects used by the projects overview.
-   */
+  /** Static list of available projects. */
   readonly projects: Project[] = PROJECTS;
 
   /**
@@ -59,9 +106,9 @@ export class Projects {
   goToNextProject(): void {
     if (!this.selectedProject) return;
 
-    const currentIndex = this.projects.findIndex(
-      (project) => project.id === this.selectedProject?.id,
-    );
+    const selectedId = this.selectedProject.id;
+
+    const currentIndex = this.projects.findIndex((project) => project.id === selectedId);
     if (currentIndex === -1) return;
 
     const nextIndex = (currentIndex + 1) % this.projects.length;
@@ -80,10 +127,10 @@ export class Projects {
 
   /**
    * Converts the project's tech stack into a list of entries including positional metadata.
-   * This is used to render separators in the template.
+   * Used to render separators in the template.
    *
    * @param project - Project providing the tech stack
-   * @returns Tech entries with `isLast` marker for rendering
+   * @returns Tech entries with `isLast` marker
    */
   getTechEntries(project: Project): ProjectTechEntry[] {
     return project.tech.map((techItem, index) => ({
